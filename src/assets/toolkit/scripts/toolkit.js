92e237cde8b8af15cd67edccb105881e6e73ec9e
Myles Braithwaite <me@mylesbraithwaite.com>
2016-01-04 14:53:13 -0500

    Did some stuff.

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,5 +1,25 @@
 /**
- * Toolkit JavaScript
+ * Old Fashioned 0.0.0
+ * Copyright 2015 Myles Braithwaite
+ * MIT License
+ * https://github.com/myles/old-fashioned/blob/master/LICENSE
  */
 
 'use strict';
+
+$(document).ready(function() {
+	var menuToggle = document.getElementById('js-mobile-menu'),
+		navigationMenu = document.getElementById('js-navigation-menu');
+	
+	navigationMenu.removeClass("show");
+	
+	menuToggle.on('click', function(e) {
+		e.preventDefault();
+		
+		navigationMenu.slideToggle(function() {
+			if (navigationMenu.is(':hidden')) {
+				navigationMenu.removeAttr('style');
+			}
+		});
+	});
+});
